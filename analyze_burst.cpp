8d5ffb11bad5ea491e250aab9e573917aacd6ad9
Rexi239 <a.plotnii@outlook.com>
2023-07-02 06:44:56 +0300

    analyze_burst.cpp - radical change of algorithm; even input (!); not tested

--- analyze_burst.cpp
+++ analyze_burst.cpp
@@ -7,18 +7,20 @@ using namespace std;
 
 int main() {
 
+    cout.precision(6);
+
     // вводим данные для генерации, генерируем
     // считываем xlabel, ylabel (из burst.txt)
 
     // freopen("burst.txt", 'r', stdin); // has to be checked
 
     vector <double> arr_time;
-    vector <double> arr_counts;
+    vector <int> arr_counts;
 
     string s1, s2;
     cin >> s1 >> s2;
 
-    // arrays work correctly
+    // even input (!)
 
     double x, y;
     while (cin >> x && cin >> y) {
@@ -41,80 +43,40 @@ int main() {
 		counter++;
     }
     double phase_zero = sum / counter;
+    // cerr << phase_zero << endl;
 
-    cerr << phase_zero << endl;
 
-    /* индексы:
-        -200 = 0
-        labelx (-200) = index (0)
-        index = labelx + 200
-    */
+    // определяем границы всплеска
+    double burst_begin_time = -97.0; // начало всплеска
+    double burst_end_time = 97.0; // конец всплеска
 
-    // определяем уровень фона - ok
+    int N = 5; // значимость детектирования
+    int k = 0; // "длина" интервала
 
-    /* double max_amplitude = phase_zero;
-    for (int i = 0; i < 125; ++i) {
-        if (arr_counts[i] > max_amplitude) {
-            max_amplitude = arr_counts[i];
-        }
-    }
-    cerr << max_amplitude << endl;
-    */
+    for (int i = 0; i < arr_counts.size(); ++i) {
+        int C_tot = 0; // полное число отсчётов на выбранном интервале
 
-    // med_amplitude
-    sum = 0;
-    counter = 0;
-    for (int i = 0; i <= 125; ++i) {
-        if (arr_counts[i] > phase_zero) {
-            sum += arr_counts[i];
-            counter++;
-        }
-    }
-    double med_amplitude = sum / counter;
-    cerr << med_amplitude << endl;
+        for (j = i; j < arr_counts.size(); ++j) {
+            k++;
 
-    // начало всплеска
+            if (arr_counts[j] > phase_zero) {
+                C_tot += arr_counts[j];
+                int C_bg = phase_zero * k; // число отсчётов от фона на выбранном интервале
+                double frac = (C_tot - C_bg) / sqrt(C_bg);
 
-    double burst_begin_time = -9797.97;
-    int burst_begin_time_i = -1;
-    for (int i = 0; i < arr_counts.size() - 2; ++i) {
-        if (arr_counts[i] > med_amplitude && arr_counts[i+1] > med_amplitude && arr_counts[i+2] > med_amplitude) {
-            burst_begin_time = arr_time[i];
-            burst_begin_time_i = i;
-        }
-    }
-
-    //  конец всплеска
-    int N = 5;
-    double burst_end_time;
-    int burst_end_time_i = -1;
-    for (int i = burst_begin_time_i; i < arr_counts.size() - 2; ++i) {
-        if (arr_counts[i] < med_amplitude && arr_counts[i+1] < med_amplitude && arr_counts[i+2] < med_amplitude) {
-            burst_end_time = arr_time[i];
-            burst_end_time_i = i;
-        }
-    }
-
-    /* Считать, что на заданном временном интервале есть значимое превышение над фоном
-    если  (C_tot - C_bg) / sqrt(C_bg) > N, где
-    C_tot - полное число отсчётов на выбранном интервале,
-    C_bg -  число отсчётов от фона на выбранном интервале,
-    N - значимость детектирования (взять N=5, в дальнейшем обсудим физический смысл этого значения). */
-
-    /* // to be fixed
-    for (int i = 400; i >= burst_begin_time; --i) {
-        int C_tot = i - burst_begin_time + 1;
-
-        for (int j = burst_begin_time; j <= i; ++j) {
-            int C_bg = i - j + 1;
-
-            if ((C_tot - C_bg) / sqrt(C_bg) > N) {
-                burst_end_time = arr_time[j];
+                if (frac > N) {
+                    burst_begin_time = arr_time[i];
+                    burst_end_time = arr_time[j];
+                }
+            }
+            else {
+                C_tot = 0;
+                k = 0;
             }
         }
-    } */
+    }
 
-	cerr << endl;
+	// cerr << endl;
     cout << burst_begin_time << endl << burst_end_time << endl;
 
     return 0;
