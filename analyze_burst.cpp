e45c358e90c895f03c368c34dda347460c609269
Rexi239 <113353063+Rexi239@users.noreply.github.com>
2023-07-28 10:26:11 +0300

    functions added, output with comments
    алгоритмы не менялись, только оформление

--- analyze_burst.cpp
+++ analyze_burst.cpp
@@ -1,57 +1,61 @@
 #include <iostream>
 #include <vector>
+#include <string>
 #include <math.h>
 
 #include <fstream>
-// #include <cstdio>
 
 using namespace std;
 
-int main() {
-    cout.precision(6);
+void read_data (
+    const string & file_name,
+    vector <double> & arr_time,
+    vector <int> & arr_counts
+) {
+    ifstream in(file_name);
 
-    // считываем arr_time, arr_counts (из burst.txt)
-    // freopen("burst.txt", 'r', stdin); // has to be checked
-
-    vector <double> arr_time;
-    vector <int> arr_counts;
-
-    ifstream in ("burst.txt"); // окрываем файл для чтения
-
-    if (in.is_open()) {
-        string s1, s2;
-        in >> s1 >> s2;
-
-        double x;
-        int y;
-        int cin_cnt = 400;
-
-        while (cin_cnt >= 0) {
-                in >> x >> y;
-                arr_time.push_back(x);
-                arr_counts.push_back(y);
-                cin_cnt--;
-        }
+    if (!in.is_open()) {
+        cout << "Error opening file: " << file_name;
+        return;
     }
+
+    string s1, s2;
+    in >> s1 >> s2;
+
+    double x;
+    int y;
+    int cin_cnt = 400;
+
+    while (cin_cnt >= 0) {
+        in >> x >> y;
+        arr_time.push_back(x);
+        arr_counts.push_back(y);
+        cin_cnt--;
+    }
+
     in.close();
+}
 
-    /* for (int i = 0; i < 125; ++i) {
-    	cerr << arr_time[i] << "    " << arr_counts[i] << endl;
-	}
-	cerr << endl; */
-
-    // определяем уровень фона
+double get_bg_level(
+    //const vector <double> & arr_time,
+    const vector <int> & arr_counts
+) {
     double sum = 0;
     int counter = 0;
     for (int i = 0; i <= 125; ++i) {
         sum += arr_counts[i];
 		counter++;
     }
-    double phase_zero = sum / counter;
-    // cerr << phase_zero << endl;
+    return sum / counter;
+}
 
+void burst_search_draft(
+    const double & bg_level,
+    const vector <double> & arr_time,
+    const vector <int> & arr_counts
+) {
 
-    // определяем границы всплеска
+    // предопределяем границы всплеска
     double burst_begin_time = -97.0; // начало всплеска
     double burst_end_time = 97.0; // конец всплеска
 
@@ -66,10 +70,10 @@ int main() {
         double C_tot = 0; // полное число отсчётов на выбранном интервале
 
         for (int j = i; j < arr_counts.size(); ++j) {
-            if (arr_counts[j] > phase_zero) {
+            if (arr_counts[j] > bg_level) {
                 length++;
                 C_tot += arr_counts[j];
-                double C_bg = phase_zero * length; // число отсчётов от фона на выбранном интервале
+                double C_bg = bg_level * length; // число отсчётов от фона на выбранном интервале
                 double frac = (C_tot - C_bg) / sqrt(C_bg);
 
                 if (frac > N) {
@@ -84,8 +88,29 @@ int main() {
         }
     }
 
-	// cerr << endl;
-    cout << burst_begin_time << endl << burst_end_time << endl;
+    cout << "Burst search results:\n";
+    cout << "Start time: " << burst_begin_time << '\n';
+    cout << "End time: " << burst_end_time << '\n';
+
+}
+
+int main() {
+    cout.precision(6);
+
+    const string file_name = "burst.txt";
+
+    vector <double> arr_time;
+    vector <int> arr_counts;
+
+    // читаем данные
+    read_data(file_name, arr_time, arr_counts);
+
+    // определяем уровень фона
+    double bg_level = get_bg_level(arr_counts); // уровень фона
+    cout << "Bg. level: " << bg_level << endl << endl;
+
+    // ищем границы всплеска
+    burst_search_draft(bg_level, arr_time, arr_counts);
 
     return 0;
 }
