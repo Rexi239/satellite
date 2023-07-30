048f5766336cd9dbc8d5fda17d5eb32ebe719f8c
Rexi239 <113353063+Rexi239@users.noreply.github.com>
2023-07-30 11:56:59 +0300

    method by DS + minor changes

--- analyze_burst.cpp
+++ analyze_burst.cpp
@@ -11,7 +11,8 @@ void read_data (
     const string & file_name,
     vector <double> & arr_time,
     vector <int> & arr_counts
-) {
+    )
+{
     cout.precision(6);
     ifstream in(file_name);
 
@@ -43,67 +44,104 @@ void read_data (
 }
 
 double get_bg_level(
-    //const vector <double> & arr_time,
+    const double & Ti, // начало интервала определения фона
+    const double & Tf, // конец интервала определения фона
+    const vector <double> & arr_time,
     const vector <int> & arr_counts
-) {
+    )
+{
+    // определяем уровень фона
     double sum = 0;
     int counter = 0;
-    for (int i = 0; i <= 125; ++i) {
+    for (int i = 0; i <= arr_time.size(); ++i) {
+        if (arr_time[i] < Ti) {continue;}
+        if (arr_time[i] > Tf) {break;}
+
         sum += arr_counts[i];
 		counter++;
     }
+
     return sum / counter;
 }
 
-void burst_search_draft(
-    const double & bg_level,
+void burst_search(
+    const double & Ti,        // начало интервала поиска всплеска
+    const double & Tf,        // конец интервала поиска всплеска
+    const double & threshold, // порог детектирования
+    const double & bg_level,  // уровень фона
     const vector <double> & arr_time,
     const vector <int> & arr_counts
-) {
+    )
+{
+    double
+        burst_begin_time, // время начала всплеска
+        burst_end_time,   // время конца всплеска
+        counts_tot,       // полное число отсчётов при поиске превышения
+        counts_bg,        // число отсчётов фона при поиске превышения
+        frac_detected,    // значимость обнаруженного всплеска
+        dt_burst;         // длителность интервала всплеска
 
-    // предопределяем границы всплеска
-    double burst_begin_time = -97.0; // начало всплеска
-    double burst_end_time = 97.0; // конец всплеска
+    bool excess_found = false;
 
-    int N = 5; // значимость детектирования
-    bool borders_found = false;
+    // ограничиваем конец интервала поиска
+    // caution! учитываем, что данные подаются с интервалом в одну секунду
+    int Tf_i = Tf - arr_time[0];
+    int i_max = Tf_i;
+
+    // общий случай (конец интервала поиска -- конец массива)
+    // int i_max = arr_counts.size();
 
     for (int i = 0; i < arr_counts.size(); ++i) {
+        if (arr_time[i] < Ti) {continue;}
+        if (arr_time[i] == Tf) {break;}
 
-        if (borders_found) break;
+        // нет смысла начинать суммировать с бина ниже фона
+        if (arr_counts[i] < bg_level) {continue;}
 
         int length = 0;
         double C_tot = 0; // полное число отсчётов на выбранном интервале
 
-        for (int j = i; j < arr_counts.size(); ++j) {
-            if (arr_counts[j] > bg_level) {
-                length++;
-                C_tot += arr_counts[j];
-                double C_bg = bg_level * length; // число отсчётов от фона на выбранном интервале
-                double frac = (C_tot - C_bg) / sqrt(C_bg);
+        for (int j = i; j < i_max; ++j) {
 
-                if (frac > N) {
-                    burst_begin_time = arr_time[i];
-                    burst_end_time = arr_time[j];
-                    borders_found = true;
-                }
-            }
-            else {
-                break;
+            length++;
+            C_tot += arr_counts[j];
+            double C_bg = bg_level * length; // число отсчётов от фона на выбранном интервале
+            double frac = (C_tot - C_bg) / sqrt(C_bg);
+
+            if (frac > threshold) {
+                burst_begin_time = arr_time[i];
+                burst_end_time = arr_time[j];
+                counts_tot = C_tot;
+                counts_bg = C_bg;
+                frac_detected = frac;
+                dt_burst = burst_end_time - burst_begin_time;
+
+                excess_found = true;
+                i_max--;
             }
         }
+
     }
 
-    cout << "Burst search results:\n";
+    cout.precision(4);
+    cout << "Burst search results:" << '\n';
     cout << "Start time: " << burst_begin_time << '\n';
     cout << "End time: " << burst_end_time << '\n';
-
+    cout << "Duration: " << dt_burst << '\n';
+    cout << "Counts tot: " << counts_tot << '\n';
+    cout << "Counts bg: " << counts_bg << '\n';
+    cout << "Significance: " << frac_detected << '\n';
 }
 
 int main() {
-    cout.precision(6);
-
     const string file_name = "burst.txt";
+
+    // определяем границы интервала поиска всплеска
+    const double data_begin_time = -125.0; // начало всплеска
+    const double data_end_time = 125.0; // конец всплеска
+    const double bg_end_time = -70.0; // конец фонового интервала
+
+    const double threshold = 5; // порог (значимость) детектирования
 
     vector <double> arr_time;
     vector <int> arr_counts;
@@ -112,11 +150,24 @@ int main() {
     read_data(file_name, arr_time, arr_counts);
 
     // определяем уровень фона
-    double bg_level = get_bg_level(arr_counts); // уровень фона
-    cout << "Bg. level: " << bg_level << endl << endl;
+    double bg_level = get_bg_level(
+        data_begin_time,
+        bg_end_time,
+        arr_time,
+        arr_counts
+    );
+    cout << "Bg. level: " << bg_level << "\n\n";
 
     // ищем границы всплеска
-    burst_search_draft(bg_level, arr_time, arr_counts);
+    burst_search(
+        bg_end_time,
+        data_end_time,
+        threshold,
+        bg_level,
+        arr_time,
+        arr_counts
+    );
+    cout << '\n';
 
     return 0;
 }
